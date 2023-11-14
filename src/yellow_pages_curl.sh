47007b80b587b781731c81a9b4e54e2167146184
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2023-11-14 17:18:10 +0200

    Create yellow_pages_curl.sh

---
+++ src/yellow_pages_curl.sh
@@ -0,0 +1,4 @@
+curl --user USERNAME:PASSWORD \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal", "url": "https://www.yellowpages.ca/bus/Ontario/North-York/The-Burger-Cellar/6835043.html"}'
